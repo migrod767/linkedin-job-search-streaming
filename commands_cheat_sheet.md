fac92eef7a423f0a40d577ea4d11751e2290d2dc
Miguel Rodriguez <mrodriguez@engage3.com>
2023-04-21 18:27:22 -0500

    add documentation to kafka, add commands cheat sheet, add a proyect README

---
+++ commands_cheat_sheet.md
@@ -0,0 +1,41 @@
+# Setup Commands
+
+## Kafka
+Run kafka container shell
+
+```docker exec -it 1c31511ce206 bash```
+
+go to ``` cd /opt/bitnami/kafka/bin```
+
+### Create Topic
+```./kafka-topics.sh --create --topic linkedin_scrapper --bootstrap-server localhost:9092```
+
+### List Topics
+```./opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092```
+
+### Describe a Topic
+```./kafka-topics.sh --describe --topic linkedin_scrapper --bootstrap-server localhost:9092```
+
+### Create Consumer
+```./kafka-console-consumer.sh --topic linkedin_scrapper --bootstrap-server localhost:9092```
+
+### Check Consumer Offset
+```./kafka-consumer-groups.sh --bootstrap-server localhost:9092  --describe --group mypythonconsumer```
+
+## Zookeeper
+
+Run zookeeper container shell
+
+```docker exec -it 1c31511ce206 bash```
+
+run command
+
+```opt/bitnami/zookeeper/bin/zkCli.sh -server localhost:2181```
+
+### List all brokers topic
+
+```ls /brokers/topics```
+
+
+
+
--- src/kafka_controller.py
+++ src/kafka_controller.py
@@ -2,29 +2,43 @@ from kafka import KafkaProducer
 import time
 
 
-class KafkaController:
+class KafkaProducer:
     def __init__(self):
         self.kafka_server = 'localhost:9092'
         self.kafka_topic = 'linkedin_scrapper'
         self.kafka_producer = KafkaProducer(bootstrap_servers=self.kafka_server)
 
-    def producer_send_message(self, msg:str):
-        # Function to send messages into Kafka
+    def producer_send_message(self, msg: str):
+        """
+        Produce send messages into Kafka
+        :param msg: Message to be sent
+        :return:
+        """
         byte_msg = bytes(msg, 'utf-8')
-        self.kafka_producer.send('linkedin_scrapper', byte_msg)\
-            .add_callback(self.success)\
+        self.kafka_producer.send('linkedin_scrapper', byte_msg) \
+            .add_callback(self.success) \
             .add_errback(self.error)
         self.kafka_producer.flush()
 
     def success(self, metadata):
+        """
+        Prints the message when it has been successful
+        :param metadata:
+        :return:
+        """
         print(metadata.topic)
 
     def error(self, exception):
+        """
+        Prints the message when and error occurred
+        :param exception:
+        :return:
+        """
         print(exception)
 
 
 if __name__ == '__main__':
-    kafka_c = KafkaController()
+    kafka_c = KafkaProducer()
 
     messages = ['{message: This is a test}',
                 '"type": "search", "file": "file_bla_bla.json", "info": "more info bla bla"',
@@ -35,4 +49,5 @@ if __name__ == '__main__':
         print(f"Sending {message}")
         kafka_c.producer_send_message(message)
         time.sleep(2)
+
 
